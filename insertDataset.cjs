24571ae0c316d922393efaf3f679861b9867da6a
Jaffran Tirta <jaffrantirta@icloud.com>
2025-08-14 13:23:10 +0800

    fix error

--- insertDataset.cjs
+++ insertDataset.cjs
@@ -1,6 +1,6 @@
-const fs = require('fs');
-const path = require('path');
-const { createClient } = require('@supabase/supabase-js');
+import fs from "fs";
+import path from "path";
+import { createClient } from '@supabase/supabase-js';
 require('dotenv').config();
 
 const supabase = createClient(
